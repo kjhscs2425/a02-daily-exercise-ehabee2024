118598780a5c814942595f40b4d8c370431202de
ehabee <ebender2025@kehillahstudent.org>
2024-09-25 11:39:51 -0700

    done

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 
 Make this shape:
 
-![Screenshot 2023-09-18 at 8](Screenshot%202023-09-18%20at%208.33.55%20AM.png)
+![Screenshot 2023-09-18 at 8]
--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import turtle
+open turtle
+move turtle(90) left
+move turtle (45)
+move turtle foward(100)
+move turtle (45)
+move turtle foward(100)
+move turtle (45)
+move turtle foward(100)
+move turtle (45)
+move turtle foward(60)
+move turtle (45)
+move turtle foward(60)
+move turtle (45)
+move turtle (45)
+move turtle (45)
+move turtle (30) 
+
